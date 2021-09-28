e9afff3a5b653e718bbeb32617d6b4928388cbd4
David Rosero Calle <rose800399@siigo.com>
2021-09-28 11:28:15 -0500

    test config

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-from .database.mongodbutils import close_mongodb_connection, connect_mongodb
+from database.mongodbutils import close_mongodb_connection, connect_mongodb
 from .api.api import api
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi import FastAPI
